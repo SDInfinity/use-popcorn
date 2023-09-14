5ce72c8ec91c560375786c808d46546ab2ad7bd4
SOUGATA DAS <crusaderbeastincarnate@gmail.com>
2023-09-14 20:25:42 +0530

    first commit.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <header className="navbar">
+      <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+        <input
+          className="search search-bar"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className="num-results">
+          Found <strong>3</strong> results
+        </p>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
